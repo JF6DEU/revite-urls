3d3529a1905fc8f19b9c32da6376cf147842cf64
JF6DEU <152010892+JF6DEU@users.noreply.github.com>
2024-07-10 19:41:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import json
 import requests
 import os
 
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+template = Jinja2Templates(directory="templates").TemplateResponse
+
+@app.get("/", response_class=HTMLResponse)
+async def ytview(request: Request):
 response = requests.get(os.environ.get('JSON_URL'))
 vm = response.json()
+return template("main.html", {brlist:str(vm)});
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
